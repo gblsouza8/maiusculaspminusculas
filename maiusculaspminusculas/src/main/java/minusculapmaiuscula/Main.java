fc4eac0cb856f6dff5b64d67c31244f6dc34fd3b
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-20 15:31:38 -0300

    Segundo Commit

--- maiusculaspminusculas/src/main/java/minusculapmaiuscula/Main.java
+++ maiusculaspminusculas/src/main/java/minusculapmaiuscula/Main.java
@@ -1,7 +1,16 @@
 package minusculapmaiuscula;
-
+import java.util.Scanner;
+// programa que transforma o texto inserido pelo usuário em um texto formado completamente com letras maiusculas usando o metódo toUpperCase
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite aqui o texto para deixar inteiro em letra maiúscula: ");
+        String texto = input.nextLine();
+        input.close();
+
+        String textoMaiusculo = texto.toUpperCase();
+        System.out.printf("Texto formatado: %s", textoMaiusculo);
+
     }
 }
